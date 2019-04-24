db2088edbe7909f991042f15e3cf2903000d1a90
James Abney <james.abney@gmail.com>
2019-04-24 16:19:36 -0700

    Move core logic into neo-core

--- src/neopass.ts
+++ src/neopass.ts
@@ -1,11 +1,16 @@
 import { IPlugin } from './plugin'
 import { PluginStore } from './plugin-store'
 import { PluginResolver } from './plugin-resolver'
-import { IValidator, IValidatorError } from './validator'
-import { IGenerator, Generator } from './generator'
-import { IPluginInfo } from './plugin-info'
-import { passwordInfo } from './password-info'
-import { runValidator } from './helpers/run-validator'
+import { IValidatorError } from './validator'
+
+import {
+  NeoCore,
+  INeoConfig,
+  PluginInfo,
+  IEvaluator,
+  PasswordStrength,
+  IGeneratorInfo
+} from './neo-core'
 
 // Plugins
 import { RandomGenerator } from './plugins/random.generator'
@@ -18,30 +23,6 @@ import { ClassesValidator } from './plugins/classes-validator'
 import { SequenceValidator } from './plugins/sequence.validator'
 import { RunValidator } from './plugins/run.validator'
 import { TopologyValidator } from './plugins/topology.validator'
-
-type PluginInfo = string|IPluginInfo
-
-interface IEvaluator {
-  weight?: number
-  validators: PluginInfo[]
-}
-
-export type PasswordStrength = [number, IValidatorError[]]
-
-// Neopass configuration interface.
-export interface INeoConfig {
-  useBuiltinGenerators?: boolean
-  useBuiltinValidators?: boolean
-  plugins?: IPlugin[]
-  validators?: PluginInfo[]
-  evaluators?: IEvaluator[]
-}
-
-// Information about a registered generator.
-export interface IGeneratorInfo {
-  name: string
-  title: string
-}
 
 // Create a default configuration.
 const _defaultConfig: INeoConfig = {
@@ -79,6 +60,11 @@ const _builtinValidators = [
 let _configuration: INeoConfig
 
 /**
+ * Core class for neo functionality.
+ */
+let _core: NeoCore
+
+/**
  *
  */
 function _registerPlugins(plugins: IPlugin[]) {
@@ -110,6 +96,8 @@ export function neopass(config?: INeoConfig|null) {
     }
 
     _configuration = _init(_config)
+
+    _core = new NeoCore(_configuration, _pluginStore, _pluginResolver)
   }
 
   return neopass
@@ -118,116 +106,38 @@ export function neopass(config?: INeoConfig|null) {
 /**
  * Generate a password using the given generator reference.
  */
-neopass.generate = function generate(len: number, generator: PluginInfo): string {
-  const _generate = _pluginResolver.resolve<Generator>('generator', generator)
-  return _generate(len)
+function generate(len: number, generator: PluginInfo): string {
+  return _core.generate(len, generator)
 }
+
+neopass.generate = generate
 
 /**
  *
  */
-neopass.evaluate = function evaluate(password: string, evaluators?: IEvaluator[]): PasswordStrength {
-  evaluators = evaluators || _configuration.evaluators
-
-  if (!Array.isArray(evaluators) || evaluators.length === 0) {
-    throw new Error('evaluate not configured correctly')
-  }
-
-  // Get the password info object.
-  const info = passwordInfo(password)
-
-  // Start with ideal strength and reduce for any validation error.
-  let strength = 1
-
-  // Get a list of warnings and do other work.
-  const warnings = evaluators.reduce((list, evaluator) => {
-    const { weight, validators } = evaluator
-
-    // Process each validator and apply errors to the strength.
-    validators.forEach((validator) => {
-      const _validator = _pluginResolver.resolve<IValidator>('validator', validator)
-      const errors = runValidator(_validator, info)
-
-      // For every error, reduce strength.
-      errors.forEach((error) => {
-        if (typeof error.score === 'number') {
-          // If the error has a score, multiply strength by the score.
-          strength *= Math.min(error.score, 1.0)
-        } else {
-          // The validator doesn't provide a score. Use weight fallback.
-          if (typeof weight !== 'number') {
-            throw new Error('no fallback weight specified in configuration')
-          }
-
-          // Check that weight is in range.
-          if (weight > 1.0 || weight < 0.0) {
-            throw new Error('wieght must be in the range [0, 1]')
-          }
-
-          // Multiply the strength by the weight.
-          strength *= weight
-        }
-      })
-
-      // strength = strength * weight ** errors.length
-      list.push.apply(list, errors)
-    })
-
-    return list
-  }, [] as IValidatorError[])
-
-  return [strength, warnings]
+function evaluate(password: string, evaluators?: IEvaluator[]): PasswordStrength {
+  return _core.evaluate(password, evaluators)
 }
+
+neopass.evaluate = evaluate
 
 /**
  * Run a validation chain against a password.
  */
-neopass.validate = function validate(
-  password: string,
-  validators?: PluginInfo[]
-): IValidatorError[] {
-    // Get validators from config if not given in arg list.
-    validators = Array.isArray(validators) && validators.length > 0
-      ? validators : _configuration.validators
-
-  // Check that we have a non-zero-length array.
-  if (!Array.isArray(validators) || validators.length === 0) {
-    throw new Error('no validators specified')
-  }
-
-  // Map supplied validator references to plugins.
-  const _validators = validators.map(validator =>
-    _pluginResolver.resolve<IValidator>('validator', validator))
-
-  // Get the password info object.
-  const info = passwordInfo(password)
-
-  /**
-   * Get a list of validation errors by executing all
-   * validators against the password and password info.
-   */
-  const errors = _validators.reduce((errList, validator) => {
-    const _errors = runValidator(validator, info)
-    return errList.push.apply(errList, _errors) && errList || errList
-  }, [] as IValidatorError[])
-
-  // Return generated errors.
-  return errors
+function validate(password: string, validators?: PluginInfo[]): IValidatorError[] {
+  return _core.validate(password, validators)
 }
+
+neopass.validate = validate
 
 /**
  * Get a list of registered generators.
  */
-neopass.generators = function generators(): IGeneratorInfo[] {
-  const generators = <IGenerator[]>_pluginStore.getAll('generator')
-
-  // Create a list of generator info objects.
-  const infoList = generators.map((gen) => {
-    return { name: gen.name, title: gen.title }
-  })
-
-  return infoList
+function generators(): IGeneratorInfo[] {
+  return _core.generators()
 }
+
+neopass.generators = generators
 
 /**
  *
