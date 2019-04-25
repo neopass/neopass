ec6e1751d3eb1b765e2cba5cc2ef704df9c3822f
James Abney <james.abney@gmail.com>
2019-04-25 15:49:42 -0700

    Create GeneratorPlugin base class

--- src/generator.ts
+++ src/generator.ts
@@ -1,8 +1,17 @@
-import { IPlugin } from './plugin'
+import { IPlugin, PluginType } from './plugin'
 
-export type Generator = (len: number) => string
+export type Generate = (len: number) => string
 
-export interface IGenerator extends IPlugin<Generator> {
+export interface IGenerator extends IPlugin<Generate> {
   readonly title: string
-  readonly fn: (options: any) => Generator
+  fn(options: any): Generate
 }
+
+export abstract class GeneratorPlugin implements IGenerator {
+  public abstract type: PluginType
+  public abstract name: string
+  public abstract title: string
+  public abstract fn(options: any): Generate
+}
+
+export type Generator = new () => GeneratorPlugin
