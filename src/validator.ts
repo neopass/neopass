779ab7845810804dae61a12a44bf0f70830f4d63
James Abney <james.abney@gmail.com>
2019-04-25 15:49:54 -0700

    Create ValidtorPlugin base class

--- src/validator.ts
+++ src/validator.ts
@@ -1,4 +1,5 @@
 import { IPasswordInfo } from './core/password-info'
+import { IPlugin, PluginType } from './plugin'
 
 export type RequestType = keyof IPasswordInfo
 
@@ -11,6 +12,14 @@ export interface IValidatorError {
 }
 
 export interface IValidator {
-  request: (RequestType)[]
+  request: RequestType[]
   validate(...args: any[]): IValidatorError[]
 }
+
+export abstract class ValidatorPlugin implements IPlugin<IValidator> {
+  public abstract type: PluginType
+  public abstract name: string
+  public abstract fn(...args: any[]): IValidator
+}
+
+export type Validator = new () => ValidatorPlugin
