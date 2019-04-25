c193f8c9e993d7ea5bf5afe39b32a19211634445
James Abney <james.abney@gmail.com>
2019-04-25 07:28:52 -0700

    Remove info property

--- src/generator.ts
+++ src/generator.ts
@@ -1,10 +1,8 @@
-import { IGeneratorInfo } from './generator-info'
 import { IPlugin } from './plugin'
 
 export type Generator = (len: number) => string
 
 export interface IGenerator extends IPlugin<Generator> {
   readonly title: string
-  readonly info?: IGeneratorInfo|null
   readonly fn: (options: any) => Generator
 }
