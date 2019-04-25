502d6f34b4f37e3b8270597e0952287033cfb5f0
James Abney <james.abney@gmail.com>
2019-04-25 13:44:09 -0700

    Embed utilities in plugin-resolver module

--- src/plugin-resolver.ts
+++ src/plugin-resolver.ts
@@ -1,7 +1,57 @@
 import { PluginStore } from './plugin-store'
 import { PluginType } from './plugin'
-import { parsePluginRef } from './utils/ref-parser'
+import regexEach from 'regex-each'
 import { IPluginInfo } from './plugin-info'
+
+const tChar = String.raw`[^ ,:=]`
+const tPlugin = String.raw`${tChar}+(?= *:|$)`
+const tKeyVal = String.raw`${tChar}+=${tChar}+`
+const tParser = String.raw`^(${tPlugin})| *(?:,|:) *(${tKeyVal})| *(?:,|:) *(${tChar}+)`
+const parser = new RegExp(tParser, 'yi')
+
+/**
+ * Try to parse a string as a json value.
+ */
+function parseJson(str: string): string {
+  try {
+    return JSON.parse(str)
+  } catch (e) {
+    return str
+  }
+}
+
+/**
+ * Parse a plugin reference into name, options, and arguments.
+ */
+function parsePluginRef(str: string): IPluginInfo {
+  const info: any = { plugin: '', args: [], options: {} }
+
+  let lastIndex = 0
+  regexEach(parser, str, (match, expr) => {
+    const [,plugin, keyval, val] = match
+
+    if (plugin != null) {
+      info.plugin = plugin
+    }
+
+    if (keyval != null) {
+      const [key, val] = keyval.split('=')
+      info.options[key] = parseJson(val)
+    }
+
+    if (val != null) {
+      info.args.push(parseJson(val))
+    }
+
+    lastIndex = expr.lastIndex
+  })
+
+  if (lastIndex < str.length) {
+    throw new Error(`could not parse plugin string "${str}"`)
+  }
+
+  return <IPluginInfo>info
+}
 
 export class PluginResolver {
   public resolve: <T = any>(type: PluginType, value: string|IPluginInfo) => T
