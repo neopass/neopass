d858c650f10fd1413754075062927f1c52c24177
James Abney <james.abney@gmail.com>
2019-04-24 22:42:09 -0700

    Add plugins/index.ts to project

---
+++ src/plugins/index.ts
@@ -0,0 +1,11 @@
+export { LettersNumbersGenerator } from './letters-numbers.generator'
+export { RandomGenerator } from './random.generator'
+
+export { ClassesValidator } from './classes.validator'
+export { DepthValidator } from './depth.validator'
+export { EntropyValidator } from './entropy.validator'
+export { LengthValidator } from './length.validator'
+export { RunValidator } from './run.validator'
+export { SequenceValidator } from './sequence.validator'
+export { ShannonValidator } from './shannon.validator'
+export { TopologyValidator } from './topology.validator'
