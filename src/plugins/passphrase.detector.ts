3cfd77d27358e8c9c519286b5ed6b88a13275fc6
James Abney <james.abney@gmail.com>
2019-04-25 21:26:38 -0700

    Implement PassphraseDetector

---
+++ src/plugins/passphrase.detector.ts
@@ -0,0 +1,28 @@
+import { PluginType } from '../plugin'
+import { DetectorPlugin, IDetector } from '../detector'
+
+export class PassphraseDetector extends DetectorPlugin {
+
+  get type(): PluginType {
+    return 'detector'
+  }
+
+  get name(): string {
+    return 'passphrase'
+  }
+
+  fn(options: any, min: number): IDetector {
+    if (typeof min !== 'number') {
+      throw new Error('passphrase detector needs a min argument')
+    }
+
+    const validator: IDetector = {
+      request: ['length'],
+      exec(length: number) {
+        return length >= min
+      }
+    }
+
+    return validator
+  }
+}
