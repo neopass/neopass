4b21f8ec760faabc6195cf1b6f25bd622fbdabf6
James Abney <james.abney@gmail.com>
2019-04-27 11:37:21 -0700

    Add evaluator spec

--- src/spec/depth-validator.spec.ts
+++ src/spec/depth-validator.spec.ts
@@ -1,13 +1,13 @@
 import assert from 'assert'
 import { NeoPass } from '../neo-pass'
-import { DepthValidator } from '../plugins';
+import { DepthValidator } from '../plugins'
 
 const neo = new NeoPass({
   plugins: [ new DepthValidator() ],
   validators: [ 'depth:95' ],
   evaluators: [
     {
-      weight: 1.0,
+      weight: 0.5,
       validators: [
         'depth:95'
       ]
@@ -22,7 +22,7 @@ describe('DepthValidator', () => {
 
     assert.strictEqual(errors.length, 1)
     assert.strictEqual(name, 'depth')
-    assert.strictEqual(score > 0, true)
+    assert.strictEqual(score > 0 && score < 1, true)
   })
 
   it('errors out if wrong argument type given', () => {
@@ -38,5 +38,21 @@ describe('DepthValidator', () => {
   it('passes if sufficient depth detected', () => {
     const errors = neo.validate('Abcd3fg$')
     assert.strictEqual(errors.length, 0)
+  })
+
+  it('works with evaluation chain', () => {
+    let result = neo.evaluate('Abcd3fg$')
+    var {strength = -1} = result
+
+    assert.strictEqual(strength, 1)
+
+    result = neo.evaluate('abc')
+    var {strength = -1, warnings} = result
+    const {score = -1} = warnings[0]
+
+    assert.strictEqual(warnings.length, 1)
+    assert.notStrictEqual(strength, -1)
+    assert.notStrictEqual(score, -1)
+    assert.strictEqual(0.5 * score, strength)
   })
 })
