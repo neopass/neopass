cdf4220ada6cb973eb157cfa22b1bd8879daf83e
James Abney <james.abney@gmail.com>
2019-04-28 10:34:39 -0700

    Move nyc config to file

---
+++ nyc.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  include: [
+    'src/**/*.ts'
+  ],
+  exclude: [
+    'src/spec'
+  ],
+  extension: [
+    '.ts'
+  ],
+  require: [
+    'ts-node/register'
+  ],
+  reporter: [
+    'lcov',
+    'text-summary'
+  ],
+  sourceMap: true,
+  instrument: true
+}
