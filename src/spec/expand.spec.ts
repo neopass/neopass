51fb11a386d0d5bef513836def2655529338bad8
James Abney <james.abney@gmail.com>
2019-04-28 09:37:40 -0700

    Impement expand spec

---
+++ src/spec/expand.spec.ts
@@ -0,0 +1,25 @@
+import assert from 'assert'
+import { expandRange, expandClass, expand } from '../utils'
+
+describe('expand', () => {
+  it('converts a range into a string', () => {
+    const chars = expandRange([97, 99])
+    assert.strictEqual(chars.join(''), 'abc')
+  })
+
+  it('converts a class into a string', () => {
+    const chars = expandClass([[97, 99], [49, 51]])
+    assert.strictEqual(chars.join(''), 'abc123')
+  })
+
+  it('converts a set into a string', () => {
+    const chars = expand([[[97, 99], [100, 102]], [[49, 49], [50, 51]], [[91, 93]]])
+    assert.strictEqual(chars.join(''), 'abcdef123[\\]')
+  })
+
+  it('throws if range too large', () => {
+    assert.throws(() => {
+      expandRange([0, 10000])
+    })
+  })
+})
