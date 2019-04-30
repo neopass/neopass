88c2d4b152e481c7c949451d4ed6a7a1f4d118e9
James Abney <james.abney@gmail.com>
2019-04-30 16:40:38 -0700

    Throw if reconfiguration attempted

--- src/neopass.ts
+++ src/neopass.ts
@@ -56,14 +56,18 @@ let _core: NeoPass
  * Neopass instance.
  */
 export function neopass(config?: INeoConfig|null) {
-  // Apply user config to default config.
-  config = {..._defaultConfig, ...config}
+  if (!_core) {
+    // Apply user config to default config.
+    config = {..._defaultConfig, ...config}
 
-  // Create the neopass core instance.
-  _core = new NeoPass(
-    config,
-    config.useBuiltinGenerators ? _builtinGenerators : null,
-    config.useBuiltinValidators ? _builtinValidators : null)
+    // Create the neopass core instance.
+    _core = new NeoPass(
+      config,
+      config.useBuiltinGenerators ? _builtinGenerators : null,
+      config.useBuiltinValidators ? _builtinValidators : null)
+  } else {
+    throw new Error('neopass is already configured')
+  }
 
   return neopass
 }
