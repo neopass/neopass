d115346f39a284a6a09f828dac565931ca50b55c
James Abney <james.abney@gmail.com>
2019-04-29 18:17:59 -0700

    Implement custom.validator.ts

---
+++ src/plugins/custom.validator.ts
@@ -0,0 +1,37 @@
+import { IValidator, ValidatorPlugin, IValidatorError } from '../validator'
+
+export class CustomValidator extends ValidatorPlugin {
+  get name(): string { return 'custom' }
+
+  configure(options: any, min: number): IValidator {
+    const fn = options.exec
+
+    const validator: IValidator = {
+      request: [ 'password', 'topology', 'classes',
+        'depth', 'length', 'entropy', 'shannon'],
+
+      // The validation logic.
+      exec(password: string, topology: string, classes: string,
+        depth: number, length: number, entropy: number, shannon: number
+      ): IValidatorError[] {
+
+        // Check that we actually have a function.
+        if (typeof fn === 'function') {
+          // Create an info object.
+          const info = {
+            password, topology, classes, depth, length, entropy, shannon}
+
+          /**
+           * Should return at least [{name, msg}] if there is an error
+           * and [] if not.
+           */
+          return fn(info)
+        }
+
+        throw new Error('custom validator function not specified')
+      }
+    }
+
+    return validator
+  }
+}
