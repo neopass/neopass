22ed4b3866d67375368fa68347acf3c4076502ff
James Abney <james.abney@gmail.com>
2019-04-29 20:56:10 -0700

    Remove redundant code

--- src/plugins/custom.validator.ts
+++ src/plugins/custom.validator.ts
@@ -3,7 +3,7 @@ import { IValidator, ValidatorPlugin, IValidatorError } from '../validator'
 export class CustomValidator extends ValidatorPlugin {
   get name(): string { return 'custom' }
 
-  configure(options: any, min: number): IValidator {
+  configure(options: any): IValidator {
     const fn = options.exec
 
     if (typeof fn !== 'function') {
@@ -18,21 +18,15 @@ export class CustomValidator extends ValidatorPlugin {
       exec(password: string, topology: string, classes: string,
         depth: number, length: number, entropy: number, shannon: number
       ): IValidatorError[] {
+        // Create an info object.
+        const info = {
+          password, topology, classes, depth, length, entropy, shannon }
 
-        // Check that we actually have a function.
-        if (typeof fn === 'function') {
-          // Create an info object.
-          const info = {
-            password, topology, classes, depth, length, entropy, shannon }
-
-          /**
-           * Should return at least [{name, msg}] if there is an error
-           * and [] if not.
-           */
-          return fn(info)
-        }
-
-        throw new Error('custom validator function not specified')
+        /**
+         * Should return at least [{name, msg}] if there is an error
+         * and [] if not.
+         */
+        return fn(info)
       }
     }
 
