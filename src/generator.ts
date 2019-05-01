8d7ea1a6a3118170beffd5c40db7f41b0eec5567
James Abney <james.abney@gmail.com>
2019-05-01 06:10:22 -0700

    Add 'units' property to IGenerator

--- src/generator.ts
+++ src/generator.ts
@@ -1,15 +1,18 @@
 import { IPlugin, PluginType } from './plugin'
 
 export type Generate = (len: number) => string
+export type GenUnits = 'char'|'byte'
 
 export interface IGenerator extends IPlugin<Generate> {
   readonly title: string
+  readonly units: GenUnits
   configure(options: any): Generate
 }
 
 export abstract class GeneratorPlugin implements IGenerator {
   public abstract name: string
   public abstract title: string
+  public abstract units: GenUnits
   public abstract configure(options: any): Generate
   public get type(): PluginType { return 'generator' }
 }
