eb2c29dac87f38c988f2bb3b8f8ee12e73a81d8f
James Abney <james.abney@gmail.com>
2019-05-01 08:33:35 -0700

    Comments

--- src/neo-pass.ts
+++ src/neo-pass.ts
@@ -7,13 +7,21 @@ import { Generator } from './generator'
 import { Validator } from './validator'
 
 /**
- *
+ * Add a list of plugins to the store.
  */
 function _registerPlugins(store: PluginStore, plugins: IPlugin[]) {
   plugins.forEach(p => store.register(p))
 }
 
+/**
+ * Implements the neopass interface.
+ */
 export class NeoPass extends NeoCore {
+  /**
+   * @param config the configuration object
+   * @param generators a list of GeneratorPlugin subclass constructors
+   * @param validators a list of ValidatorPlugin subclass constructors
+   */
   constructor(
     config?: null|IBaseConfig, generators?: null|Generator[], validators?: null|Validator[]
   ) {
